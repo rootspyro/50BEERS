227bfd994ba73cf196dfd28d137ceffe73b284d5
rootspyro <root.spyro@gmail.com>
2024-08-29 01:15:09 -0400

    Feat: Add fonts and colors on taildinw

--- blog/tailwind.config.mjs
+++ blog/tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+    extend: {
+      colors: {
+        "dark": "#242424",
+        "base": "#FFFDF6",
+        "light": "#FAFAFA"
+      },
+      fontFamily: {
+        "title": ["Anton", 'sans-serif'],
+        "content": ["Roboto Condensed Variable", "sans-serif"]
+      }
+    },
 	},
 	plugins: [],
 }
