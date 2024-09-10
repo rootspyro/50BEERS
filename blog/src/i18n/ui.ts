4d10939faa9998c9f6a2b041e8d50f09305c68fc
rootspyro <root.spyro@gmail.com>
2024-09-10 13:04:54 -0400

    Feat: Translate footer

--- blog/src/i18n/ui.ts
+++ blog/src/i18n/ui.ts
@@ -9,11 +9,27 @@ export const ui = {
   es: {
     'nav.login': "Iniciar Sesión",
     'nav.signup': "Registrarse",
-    'nav.logout': "Cerrar Sesión"
+    'nav.logout': "Cerrar Sesión",
+    'footer.subtitle.1': "SOLO",
+    'footer.subtitle.2': "BEBIENDO",
+    'footer.subtitle.3': "ESCRIBIENDO PARA PARECER OCUPADO",
+    'footer.newsletter.text': "Mantente al día de las últimas cervezas y tragos.",
+    'footer.newsletter.placeholder': 'Correo electrónico',
+    'footer.newsletter.cta': 'Subscrirse',
+    'footer.rights': 'Todos los derechos reservados',
+    'footer.developer': 'Desarrollado por',
   },
   en: {
     'nav.login': "Login",
     'nav.signup': "Sign Up",
     'nav.logout': "Logout",
+    'footer.subtitle.1': "JUST",
+    'footer.subtitle.2': "DRINKING",
+    'footer.subtitle.3': "WRITING TO LOOK BUSY",
+    'footer.newsletter.text': "Stay in the loop with the Latest Brews and Drinks!",
+    'footer.newsletter.placeholder': 'Email',
+    'footer.newsletter.cta': 'Subscribe',
+    'footer.rights': 'All rights reserved',
+    'footer.developer': 'Developed by',
   }
 } as const;
