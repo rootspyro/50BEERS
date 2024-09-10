8c0239661cccb69e73962f2b6f70a34ba9988ea8
rootspyro <root.spyro@gmail.com>
2024-09-09 23:57:22 -0400

    Feat: init i18n directory

---
+++ blog/src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  es: "Spanish",
+  en: "English"
+}
+
+export const defaultLang = "en";
+
+export const ui = {
+  es: {
+    'nav.login': "Iniciar Sesión",
+    'nav.signup': "Registrarse"
+  },
+  en: {
+    'nav.login': "Login",
+    'nav.signup': "Sign Up"
+  }
+} as const;
