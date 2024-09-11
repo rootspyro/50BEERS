e1d0929b7cf3dde09d48dbdbd33a549553362ada
rootspyro <root.spyro@gmail.com>
2024-09-11 01:45:16 -0400

    Feat: Update location models and seeder

--- backend/db/models/location.model.go
+++ backend/db/models/location.model.go
@@ -4,17 +4,22 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Location struct {
 	ID        primitive.ObjectID `bson:"_id"`
-	Name      string             `bson:"name"`
+	EN        LocationLang       `bson:"en"`
+	ES        LocationLang       `bson:"es"`
 	URL       string             `bson:"url"`
-	Comments  string             `bson:"comments"`
 	CreatedAt string             `bson:"created_at"`
 	UpdatedAt string             `bson:"updated_at"`
 }
 
 type NewLocation struct {
-	Name      string `bson:"name"`
-	URL       string `bson:"url"`
-	Comments  string `bson:"comments"`
-	CreatedAt string `bson:"created_at"`
-	UpdatedAt string `bson:"updated_at"`
+	EN        LocationLang `bson:"en"`
+	ES        LocationLang `bson:"es"`
+	URL       string       `bson:"url"`
+	CreatedAt string       `bson:"created_at"`
+	UpdatedAt string       `bson:"updated_at"`
+}
+
+type LocationLang struct {
+	Name     string `bson:"name"`
+	Comments string `bson:"comments"`
 }
